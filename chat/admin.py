9abb872cd8ab98aab1d1032d572583ef9e066660
soaib <soaib.softdev@gmail.com>
2026-03-01 11:48:16 +0600

    Register ChatMessage model in admin with display and search configurations

--- chat/admin.py
+++ chat/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from chat.models import Connection
+from chat.models import Connection, ChatMessage
 @admin.register(Connection)
 class ConnectionAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'friend', 'created_at')
     search_fields = ('user__username', 'friend__username')
     list_filter = ('created_at',)
+
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'room_name', 'user', 'message', 'timestamp')
+    search_fields = ('room_name', 'user__username', 'message')
+    list_filter = ('timestamp',)
