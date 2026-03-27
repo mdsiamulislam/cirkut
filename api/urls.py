7dfa6f0940974ddbc8b9bb1342ad034adfad0a8d
soaib <soaib.softdev@gmail.com>
2026-03-27 13:50:05 +0600

    Add connection deletion functionality with ConnectionActionView

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from connection.views.connections import ConnectionsView
+from connection.views.connections import ConnectionsView, ConnectionActionView
 from connection.views.connection_request_view import ConnectionRequestView, ConnectionRequestActionView
 
 
 urlpatterns = [
     path('v1/connections/', ConnectionsView.as_view(), name='connections'),
+    path('v1/connections/<int:connection_id>/', ConnectionActionView.as_view(), name='delete-connection'),
     path('v1/received-requests/', ConnectionRequestView.as_view(), name='connection-requests'),
     path('v1/received-requests/<int:request_id>/', ConnectionRequestActionView.as_view(), name='accept-connection-request'),
     
--- connection/views/connections.py
+++ connection/views/connections.py
@@ -14,5 +14,17 @@ class ConnectionsView(APIView):
         user = request.user
         received_requests = ConnectionRequest.objects.filter(receiver=user, status='accepted')
         received_serializer = ConnectionRequestSerializer(received_requests, many=True)
-        
         return Response(received_serializer.data, status=status.HTTP_200_OK)
+    
+
+class ConnectionActionView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, connection_id):
+        user = request.user
+        try:
+            connection_request = ConnectionRequest.objects.get(id=connection_id, receiver=user, status='accepted')
+            connection_request.delete()
+            return Response({'detail': 'Connection request deleted.'}, status=status.HTTP_200_OK)
+        except ConnectionRequest.DoesNotExist:
+            return Response({'detail': 'Connection request not found.'}, status=status.HTTP_404_NOT_FOUND)
